764a6d48beb573283273160b7d3a56259d241199
hakuthewizard <hakuthegeek@gmail.com>
2022-08-17 17:04:55 +0200

    mini game

---
+++ app.js
@@ -0,0 +1,74 @@
+let loading = [
+    'Retrieving your message with good vibes...',
+    'Getting your fortune...',
+    'Checking the vibes...',
+    'Stay present...',
+    'Before you eat the cookie...'
+
+];
+let responses = [
+    'Things are about to get so much better',
+    'Future looks bright if you keep working hard',
+    'Whatever is meant for you is on its way',
+    'Make sure to do the undone things',
+    'Do not quit now, you might almost be there'
+];
+
+
+
+
+const container =document.querySelector("#container");
+const fortuneButton = document.querySelector("#fortuneButton");
+const theFortune = document.querySelector("#theFortune");
+
+
+
+fortuneButton.addEventListener('click', getFortune);
+
+
+
+let timeout;
+
+function getFortune () {
+
+theFortune.style.backgroundColor = "white"
+theFortune.style.color = "black"
+shuffle(loading);
+theFortune.textContent = loading[0];
+
+
+clearTimeout(timeout);
+crackCookie();
+timeout = setTimeout (function(){
+shuffle(responses);
+theFortune.textContent = responses[0];
+},2000)
+
+} 
+
+const fortuneCookie = document.querySelector("#fortuneCookie");
+const crackCookie = function() {
+  if (fortuneCookie.dataset.image == "one") {
+    fortuneCookie.src = "cracked.png";
+    fortuneCookie.dataset.image = "two";
+    return
+  } }
+//Helper function that uses Fisher-Yates algorithm-> https://stackoverflow.com/questions/2450954/how-to-randomize-shuffle-a-javascript-array/2450976#2450976
+function shuffle (array) {
+
+let currentIndex = array.length;
+let temporaryValue, randomIndex;
+
+// While there remain elements to shuffle...
+while (0 !== currentIndex) {
+// Pick a remaining element...
+randomIndex = Math.floor(Math.random() * currentIndex);
+currentIndex -= 1;
+
+// And swap it with the current element.
+temporaryValue = array[currentIndex];
+array[currentIndex] = array[randomIndex];
+array[randomIndex] = temporaryValue;
+}
+
+}
